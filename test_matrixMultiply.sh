613fcb4b15075b7d48a117422df19eebb22e7bae
tompoek <tommy.poek@outlook.com>
2024-08-26 11:07:51 +1000

    Option to profile with valgrind cachegrind

--- test_matrixMultiply.sh
+++ test_matrixMultiply.sh
@@ -3,3 +3,4 @@ set -o errexit
 rm -f test_matrixMultiply.o matrixMultiply.o 
 g++ -o2 test_matrixMultiply.cpp matrixMultiply.cpp -o test_matrixMultiply.o -mavx -fopenmp
 ./test_matrixMultiply.o
+# valgrind --tool=cachegrind ./test_matrixMultiply.o
